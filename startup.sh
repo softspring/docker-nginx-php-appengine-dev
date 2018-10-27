d2e80b2e0b9408245710fb21ce1b4bd794cdd433
Javi H. Gil <javihgil@gmail.com>
2018-10-27 16:06:23 +0200

    Propagate environment variables in startup.sh script

--- startup.sh
+++ startup.sh
@@ -4,14 +4,15 @@ export TERM=xterm
 
 if [ "$XDEBUG_ENABLED" = "1" ]
 then
+    echo "Enabling Xdebug for PHP 7.2"
     sudo bash -c 'echo "zend_extension=xdebug.so" >> /opt/php72/lib/php.ini'
     sudo bash -c 'echo "xdebug.default_enable=1" >> /opt/php72/lib/php.ini'
     sudo bash -c 'echo "xdebug.remote_enable=1" >> /opt/php72/lib/php.ini'
     sudo bash -c 'echo "xdebug.remote_autostart=1" >> /opt/php72/lib/php.ini'
     sudo bash -c 'echo "xdebug.remote_connect_back=1" >> /opt/php72/lib/php.ini'
-    sudo bash -c 'echo "xdebug.remote_host=$XDEBUG_REMOTE_HOST" >> /opt/php72/lib/php.ini'
-    sudo bash -c 'echo "xdebug.remote_port=$XDEBUG_REMOTE_PORT" >> /opt/php72/lib/php.ini'
-    sudo bash -c 'echo "xdebug.idekey=$XDEBUG_IDEKEY" >> /opt/php72/lib/php.ini'
+    sudo XDEBUG_REMOTE_HOST=$XDEBUG_REMOTE_HOST bash -c 'echo "xdebug.remote_host=$XDEBUG_REMOTE_HOST" >> /opt/php72/lib/php.ini'
+    sudo XDEBUG_REMOTE_PORT=$XDEBUG_REMOTE_PORT bash -c 'echo "xdebug.remote_port=$XDEBUG_REMOTE_PORT" >> /opt/php72/lib/php.ini'
+    sudo XDEBUG_IDEKEY=$XDEBUG_IDEKEY bash -c 'echo "xdebug.idekey=$XDEBUG_IDEKEY" >> /opt/php72/lib/php.ini'
 fi
 
 /opt/php72/bin/php /usr/local/bin/composer install --no-progress --dev
